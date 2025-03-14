30a77378b893d7e4455c4034df9523339f4dcbdc
rob <rdeaswork@gmail.com>
2025-03-14 21:59:50 +0000

    add licence header

--- src/main/java/tech/robd/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/tech/robd/jwt/filter/JwtAuthenticationFilter.java
@@ -1,9 +1,36 @@
+/*
+ * Copyright (c) 2025 Robert Deas
+ *
+ * This file is dual-licensed under the MIT License and the Apache License, Version 2.0.
+ * You may choose either license to govern your use of this file.
+ *
+ * MIT License:
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   [Insert full MIT license text or refer to a LICENSE file]
+ *
+ * Apache License, Version 2.0:
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at:
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * SPDX-License-Identifier: MIT OR Apache-2.0
+ */
 package tech.robd.jwt.filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
 import tech.robd.jwt.util.JwtTokenUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
--- src/test/java/tech/robd/jwt/controller/AuthControllerTest.java
+++ src/test/java/tech/robd/jwt/controller/AuthControllerTest.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2025 Robert Deas
+ *
+ * This file is dual-licensed under the MIT License and the Apache License, Version 2.0.
+ * You may choose either license to govern your use of this file.
+ *
+ * MIT License:
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   [Insert full MIT license text or refer to a LICENSE file]
+ *
+ * Apache License, Version 2.0:
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at:
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * SPDX-License-Identifier: MIT OR Apache-2.0
+ */
 package tech.robd.jwt.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
